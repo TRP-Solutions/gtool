a249d3251be427cc2987e49d7494c2037d907b39
JacobNeergaard <neergaard@kananga.dk>
2022-11-22 13:26:15 +0100

    allow parent config

--- header.php
+++ header.php
@@ -1,5 +1,10 @@
 <?php
-require_once __DIR__.'/config.php';
+if(file_exists(__DIR__.'/config.php')) {
+	require_once __DIR__.'/config.php';
+}
+if(file_exists(__DIR__.'/../gtool-config.php')) {
+	require_once __DIR__.'/../gtool-config.php';
+}
 require_once __DIR__.'/lib/heal-document/HealDocument.php';
 require_once __DIR__.'/lib/ufo-ajax/ufo.php';
 require_once __DIR__.'/design.php';
